a0043fb3b5ff7ca83f75738afd4c8b14c9c38756
Teresa Furr <harleygirley97@gmail.com>
2025-12-01 06:53:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9511907405-4ef32",
+  "appId": "1:681226570708:web:052f4aa90e74ec9d456441",
+  "apiKey": "AIzaSyBnle5X2L5Oh6pcov2AehPw264zRxrmtEM",
+  "authDomain": "studio-9511907405-4ef32.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "681226570708"
+};
