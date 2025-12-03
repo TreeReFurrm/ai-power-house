0a2dc1d84be8e2ff0ce7cafd91b040140e7c8431
Teresa Furr <harleygirley97@gmail.com>
2025-12-03 11:28:31 +0000

    Firebase logo Project Overview
    ReFurrm Scan
    Project settings
    General
    C

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-4760471421-5f44b",
-  "appId": "1:956004586954:web:cdf84e08a86dc6a1ab2b33",
   "apiKey": "AIzaSyDqXgddsK4diyTF-xKOSBe2cuImMeRBvis",
   "authDomain": "studio-4760471421-5f44b.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "956004586954"
+  "projectId": "studio-4760471421-5f44b",
+  "storageBucket": "studio-4760471421-5f44b.appspot.com",
+  "messagingSenderId": "956004586954",
+  "appId": "1:956004586954:web:cdf84e08a86dc6a1ab2b33",
+  "measurementId": ""
 };
