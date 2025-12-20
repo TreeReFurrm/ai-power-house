646cb1ba0d0411a821000c40334ff3fb47e9516e
Teresa Furr <harleygirley97@gmail.com>
2025-12-20 17:19:25 +0000

    oh no what happened?

--- src/app/(app)/writer/refurr-ink/page.tsx
+++
@@ -1,9 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-relevant-multimedia.ts';
-import '@/ai/flows/generate-course-outline.ts';
-import '@/ai/flows/edit-course-content.ts';
-import '@/ai/flows/write-personalized-letter.ts';
-import '@/ai/flows/summarize-document.ts';
-import '@/ai/flows/generate-business-form.ts';
